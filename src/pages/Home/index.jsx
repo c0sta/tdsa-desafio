175e7ecbb65666f0bb38dd3b54a165e8dd107566
gabriel.moura <gabriel.moura@gsw.com.br>
2021-01-22 03:09:12 -0300

    :art: Requisição em api isolada em serviço

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,55 +1,61 @@
-import { Button, Container, Grid, Modal } from "@material-ui/core";
+import { Button, Container, Grid } from "@material-ui/core";
 import React, { useState } from "react";
 import CollapsiblePost from "../../components/CollapsiblePost";
+import { getAllPosts } from "../../services/postService";
 
-import PostsTable from "../../components/PostsTable";
+import Table from "../../components/Table";
+import { makeStyles } from "@material-ui/core/styles";
+import { AddPost } from "../AddPost";
+import { ModalContext } from "../../context/modalContext";
+import Header from "../../components/Header";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    alignSelf: "right",
+  },
+}));
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
-  const [openModal, setOpenModal] = useState(false);
+  const { modalState, modalDispatch } = React.useContext(ModalContext);
+  const styles = useStyles();
 
   React.useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((posts) => {
-        console.log(posts);
-        setPosts(posts);
-      });
-    return () => {};
+    (() => getAllPosts().then((postsArray) => setPosts(postsArray)))();
   }, []);
-
-  const handleOpen = () => {
-    setOpenModal(true);
-  };
-
-  const handleClose = () => {
-    setOpenModal(false);
-  };
 
   return (
     <>
-      <Container maxWidth="sm">
-        <Grid container direction="row" justify="flex-end" alignItems="center">
-          <Button variant="contained" color="primary" onClick={handleOpen}>
+      <Header />
+      <Container maxWidth="md" className={styles.container}>
+        <Grid container direction="row">
+          <Button
+            className={styles.button}
+            variant="contained"
+            color="primary"
+            onClick={() =>
+              modalDispatch({
+                type: "open",
+                payload: "Novo Post",
+              })
+            }
+          >
             Adicionar post
           </Button>
-          <PostsTable tableHeaderTitles={["Post", "Texto"]}>
+
+          <Table tableHeaderTitles={["Post", "Texto"]}>
             {posts.map((post) => (
               <CollapsiblePost key={post.id} post={post} />
             ))}
-          </PostsTable>
+          </Table>
+          {modalState.isOpen ? <AddPost /> : null}
         </Grid>
       </Container>
-      {openModal ? (
-        <Modal
-          open={openModal}
-          onClose={handleClose}
-          aria-labelledby="simple-modal-title"
-          aria-describedby="simple-modal-description"
-        >
-          <h1>testando</h1>
-        </Modal>
-      ) : null}
     </>
   );
 }
--- src/services/postService.js
+++ src/services/postService.js
@@ -1,6 +1,9 @@
+import PropTypes from "prop-types";
+
 const apiPath = "https://jsonplaceholder.typicode.com/";
 const getAllPostsUrl = apiPath + "posts";
 const getPostUrl = apiPath + "post/";
+const createPostUrl = apiPath + "posts";
 
 function getAllPosts() {
   return fetch(getAllPostsUrl)
@@ -14,4 +17,35 @@ function getPost(id) {
     .then((postObject) => console.log(postObject));
 }
 
-export { getAllPosts, getPost };
+function create(data) {
+  return fetch(createPostUrl, {
+    method: "POST", // or 'PUT'
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return { data };
+    });
+}
+
+function update(id) {
+  return id;
+}
+
+export { getAllPosts, getPost, create };
+
+getPost.propTypes = {
+  id: PropTypes.number,
+};
+update.propTypes = {
+  id: PropTypes.number,
+};
+create.propTypes = {
+  data: PropTypes.shape({
+    post: PropTypes.string,
+    title: PropTypes.string,
+  }),
+};
