34322076ba92d7d5d989fe6dd1447b15004f1f1f
gabriel.moura <gabriel.moura@gsw.com.br>
2021-01-22 03:03:47 -0300

    :sparkles: Modal de novo post

---
+++ src/pages/AddPost/index.jsx
@@ -0,0 +1,12 @@
+// import { makeStyles } from "@material-ui/core";
+import React from "react";
+import Modal from "../../components/Modal";
+import { PostForm } from "../../components/PostForm";
+
+export const AddPost = () => {
+  return (
+    <Modal>
+      <PostForm />
+    </Modal>
+  );
+};
