341ecf87e46ad3ac45d9f3c4cccc4704fffba2df
Gabriel Costa Moura <gabriel.moura@gsw.com.br>
2021-01-23 19:59:34 -0300

    :fire: remoção do default export

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { AppBar, IconButton, Toolbar, Typography } from "@material-ui/core";
 
-export default function Header() {
+export function Header() {
   return (
     <AppBar position="static">
       <Toolbar variant="dense">
