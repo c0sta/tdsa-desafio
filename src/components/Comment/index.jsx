27b50d34d490a598c291742f8684367dd7885855
Gabriel Costa Moura <gabriel.moura@gsw.com.br>
2021-01-23 19:46:03 -0300

    :zap: Componente isolado para comentário

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -10,18 +10,31 @@ import {
 } from "@material-ui/core";
 
 import React from "react";
+
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: "100%",
-    maxWidth: "36ch",
+    width: "50%",
     backgroundColor: theme.palette.background.paper,
   },
-  inline: {
-    display: "inline",
+  titleContainer: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 14,
+    color: "#888",
+  },
+  body: {
+    fontSize: 14,
+    color: "#333",
   },
 }));
 
-export const Comment = ({ commentData }) => {
+export const Comment = ({ data }) => {
   const styles = useStyles();
   return (
     <>
@@ -32,20 +45,37 @@ export const Comment = ({ commentData }) => {
             src="https://i.pinimg.com/236x/50/e7/b9/50e7b9e3b515c09f11365487a7336f8f.jpg"
           />
         </ListItemAvatar>
+
         <ListItemText
-          primary={commentData.name}
+          primary={
+            <Box className={styles.titleContainer}>
+              <Typography
+                component="span"
+                variant="h6"
+                className={styles.title}
+                color="textPrimary"
+              >
+                <strong>{data.name}</strong>
+              </Typography>
+              <Typography
+                component="p"
+                variant="subtitle1"
+                className={styles.subtitle}
+                color="textPrimary"
+              >
+                {data.email}
+              </Typography>
+            </Box>
+          }
           secondary={
             <Box>
               <Typography
-                component="span"
-                variant="body2"
-                className={styles.inline}
+                component="p"
+                variant=""
+                className={styles.body}
                 color="textPrimary"
               >
-                {commentData.email}
-              </Typography>
-              <Typography component="span" variant="body2" color="textPrimary">
-                {commentData.body}
+                {data.body}
               </Typography>
             </Box>
           }
