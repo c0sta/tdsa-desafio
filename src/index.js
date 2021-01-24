56d5f3b1ad2e386fcf62fbb1556ba185a55d9853
Gabriel Costa Moura <gabriel.moura@gsw.com.br>
2021-01-24 17:32:00 -0300

    :bug: Prod env não esta carregando

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import React, { Suspense } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 ReactDOM.render(
   <React.StrictMode>
-    <Suspense fallback={<div>Loading...</div>}>
-      <App />
-    </Suspense>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
